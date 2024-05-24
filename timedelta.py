847e7444333ba24520e64e1a2e695846e3d2c8b6
Liudmyla <luda.skopenko80@gmail.com>
2024-05-24 23:55:20 +0300

    update timedelta

---
+++ timedelta.py
@@ -0,0 +1,10 @@
+#Якщо відняти від одного datetime об'єкту інший, то отримаємо timedelta об'єкт.
+# Він відповідає за відрізок часу між двома датами.
+from datetime import datetime
+
+seventh_day_2019 = datetime(year=2019, month=1, day=7, hour=14)
+seventh_day_2020 = datetime(year=2020, month=1, day=7, hour=14)
+
+difference = seventh_day_2020 - seventh_day_2019
+print(difference)                   # 365 days, 0:00:00
+print(difference.total_seconds())   # 31536000.
