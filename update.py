85c5a7103267dca1e349a69bb5f34e5fed8a409b
Liudmyla <luda.skopenko80@gmail.com>
2024-05-26 00:10:14 +0300

    added timedelta

--- update.py
+++ update.py
@@ -1,7 +1,15 @@
 
-from datetime import datetime
+from datetime import datetime,timedelta
 
 d = datetime(year=2000, month=1, day=28,hour=17,minute=50)
 print(d)
 a = d.strftime('%Y.%B.%A.%H.%M')
 print(a)
+now = datetime.now()
+interval = timedelta(days=10)
+result_date = now - interval
+print(result_date)
+
+interval = timedelta(days=20)
+result_date = now + interval
+print(result_date)
