1b07c7d411aa940b4072a95543ed418e042cd275
Liudmyla <luda.skopenko80@gmail.com>
2024-05-25 18:08:55 +0300

    added replace()

--- start.py
+++ start.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 
-tz = '25.05.2024'
+tz = '25.05.2022'
 d = datetime.strptime(tz, '%d.%m.%Y')
 print(type(d))
 print(f'Рік та дата : {d}')
 print(f'Число : {d.day} \nРік : {d.year} \nМісяць : {d.month}')
+temp_replace = d.replace(year=2024, month=8, day=16, hour=15, minute=25)
+print(temp_replace)
 
-
+# метод replace() повертає  новий об'єкт datetime зі зміненими   атрибутами.
--- timedelta.py
+++ timedelta.py
@@ -6,5 +6,6 @@ seventh_day_2019 = datetime(year=2019, month=1, day=7, hour=14)
 seventh_day_2020 = datetime(year=2024, month=5, day=1, hour=1)
 
 difference = seventh_day_2020 - seventh_day_2019
+
 print(difference)                   # 365 days, 0:00:00
 print(difference.total_seconds())   # 31536000.
