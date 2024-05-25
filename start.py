6e56b6fa2d4aed9da431e3fbdb5589966fc01bf6
Liudmyla <luda.skopenko80@gmail.com>
2024-05-25 17:29:46 +0300

    update strptime

--- start.py
+++ start.py
@@ -1,11 +1,10 @@
 from datetime import datetime
 
-right_now = datetime.now()
-print(f' ПОТОЧНИЙ РІК І ЧАС: {right_now}')
-print(f'ПОТОЧНИЙ РІК: {right_now.year}')
-print(f'ПОТОЧНИЙ МІСЯЦЬ: {right_now.month}')
-print(f'ПОТОЧНИЙ ДЕНЬ: {right_now.day}')
-print(f'ПОТОЧНИЙ  ЧАС: {right_now.hour}')
-print(f'ПОТОЧНІ МИНУТИ: {right_now.minute}')
-print(f'ПОТОЧНИЙ СЕКУНДИ: {right_now.second}')
-print(f'ПОТОЧНИЙ МІКРОСЕКУНДИ: {right_now.microsecond}')
+
+tz = '25.05.2024'
+d = datetime.strptime(tz, '%d.%m.%Y')
+print(type(d))
+print(f'Рік та дата : {d}')
+print(f'Число : {d.day} \nРік : {d.year} \nМісяць : {d.month}')
+
+
