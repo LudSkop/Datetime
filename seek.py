030598788ec31717fc1185a7bbba46fd50ca5606
Liudmyla <luda.skopenko80@gmail.com>
2024-05-26 23:31:26 +0300

    added get_birthday

--- seek.py
+++ seek.py
@@ -1,6 +1,15 @@
 # Щоб створити об'єкт datetime з будь-якою датою.
 from datetime import datetime
 
-d1 = datetime(year=2012, month=1, day=7, hour=14)
-print(d1) # 2012-01-07 14:00:00
 
+# d1 = datetime(year=2012, month=1, day=7, hour=14)
+# print(d1) # 2012-01-07 14:00:00
+def get_birthday():
+    name = input(f'Введіть рік.місяць.день свого дня народження:  ')
+    result = datetime.strptime(name, '%Y.%m.%d')
+    month_day = result.strftime('%B.%A')
+    print(month_day)
+
+
+if __name__ == "__main__":
+    get_birthday()
