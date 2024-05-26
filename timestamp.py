fd1c7b80e3b7b1d25a11caae457cb2f07494c0c9
Liudmyla <luda.skopenko80@gmail.com>
2024-05-27 00:38:06 +0300

    added timestamp()

--- timestamp.py
+++ timestamp.py
@@ -3,3 +3,10 @@ from  datetime import datetime
 
 date = datetime.now()
 print(f'Час в секундах з початку епохи:{date.timestamp()}')
+
+
+hour = 1716758017.135023
+hour_now = datetime.fromtimestamp(hour)
+print(hour_now)
+str_hour_now = hour_now.strftime('%d-%m-%Y, %H:%M:%S')
+print(str_hour_now)
