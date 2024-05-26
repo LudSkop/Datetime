37ae0112a1ed2e2646999a14e0af71cf902056eb
Liudmyla <luda.skopenko80@gmail.com>
2024-05-27 00:12:21 +0300

    added timestamp

--- seek.py
+++ seek.py
@@ -1,12 +1,10 @@
 # Щоб створити об'єкт datetime з будь-якою датою.
 from datetime import datetime
-
-
 # d1 = datetime(year=2012, month=1, day=7, hour=14)
 # print(d1) # 2012-01-07 14:00:00
 def get_birthday():
-    name = input(f'Введіть рік.місяць.день свого дня народження:  ')
-    result = datetime.strptime(name, '%Y.%m.%d')
+    date = input(f'Введіть рік.місяць.день свого дня народження:  ')
+    result = datetime.strptime(date, '%Y.%m.%d')
     month_day = result.strftime('%B.%A')
     print(month_day)
 
---
+++ timestamp.py
@@ -0,0 +1,5 @@
+from  datetime import datetime
+
+
+date = datetime.now()
+print(f'Час в секундах з початку епохи:{date.timestamp()}')
