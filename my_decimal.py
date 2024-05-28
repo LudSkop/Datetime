80d240a66dd98022edec3052e1913a6935186258
Liudmyla <luda.skopenko80@gmail.com>
2024-05-28 17:54:59 +0300

    added getcontaxt().prec

--- my_decimal.py
+++ my_decimal.py
@@ -6,5 +6,12 @@ print(numbers)
 result = Decimal('0.2') + Decimal('0.1') + Decimal('0.3') - Decimal('0.5')
 print(result)
 
-n = Decimal('1') / Decimal('3')
+n = Decimal('10') / Decimal('3')
 print(n)
+print(type(n))
+# В методі getcontext є атрибут .prec  він керує точністю обчислення.
+getcontext().prec = 6
+n = Decimal('10') / Decimal('3')  # 3.33333
+print(n)
+n = Decimal('1') / Decimal('3')
+print(n)                         # 0.333333
