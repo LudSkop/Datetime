f5fc769ef73b1e52f409bb6ba8475ea67fe88627
Liudmyla <luda.skopenko80@gmail.com>
2024-05-30 00:55:09 +0300

    added my_collections

--- my_collections.py
+++ my_collections.py
@@ -15,3 +15,10 @@ print(dog_object.nickname)
 print(dog_object.age)
 print(dog_object.owner)
 print(type(dog_object))
+
+temp = [1.3, 0.0, -0.5, -0.5, 0.0, -1.3, -1.3, -1.3]
+tem = collections.Counter(temp)
+print(tem)                #Counter({-1.3: 3, 0.0: 2, -0.5: 2, 1.3: 1})
+print(tem.most_common(3)) #[(-1.3, 3), (0.0, 2), (-0.5, 2)]
+print(tem.most_common(1)) #[(-1.3, 3)]
+
