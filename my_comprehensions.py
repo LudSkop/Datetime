f00c2c8548bd6440ff6cee287421e431223808b4
Liudmyla <luda.skopenko80@gmail.com>
2024-05-30 20:22:35 +0300

    added Comprehensions.

---
+++ my_comprehensions.py
@@ -0,0 +1,11 @@
+"""
+list Comprehensions[] дозволяє нам в один рядок заповнити
+список простими або складними значеннями.
+"""
+even_nums = []
+for x in range(21):
+    if x % 2 == 0:
+        even_nums.append(x)
+print(even_nums)
+a = [element for element in range(21) if element % 2 == 0]
+print(a)
--- my_random.py
+++ my_random.py
@@ -1,3 +1,20 @@
+from collections import deque
+
+l = list(range(1, 10))
+l_deque = deque(l)
+print(l_deque)
+right_deque = deque(l, 7)
+print(right_deque)
+right_deque.append(11)
+print(right_deque)
+right_deque.appendleft(100)
+print(right_deque)
+right_deque.reverse()
+print(right_deque)
+right_deque.rotate(3)
+print(right_deque)
+
+
 
 
 
